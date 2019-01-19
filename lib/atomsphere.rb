db72bed8b3761b806d775ca9a52ef8b00e346adf
Warren Guy <warren@guy.net.au>
2019-01-20 00:48:54 +0800

    v0.1.11

--- lib/atomsphere.rb
+++ lib/atomsphere.rb
@@ -1,6 +1,6 @@
 # @author Warren Guy
 module Atomsphere
-  VERSION = '0.1.10'
+  VERSION = '0.1.11'
   ROOT = "#{File.expand_path(__dir__)}/atomsphere"
 
   %w(configuration query api action).each{ |m| require "#{ROOT}/#{m}" }
